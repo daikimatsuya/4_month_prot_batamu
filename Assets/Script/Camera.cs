4f62522246a6962a2968c983ad9c3ca7ae52836c
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2024-02-28 12:04:27 +0900

    カメラ縦追従

--- Assets/Script/Camera.cs
+++ Assets/Script/Camera.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
     Transform tf;
     private RectTransform playerpos;
+    public float cameraposDiff;
 
     private void CameraController()
     {
@@ -14,6 +16,14 @@ public class Camera : MonoBehaviour
     private void Move()
     {
         tf.position = new Vector3(playerpos.position.x, tf.position.y, tf.position.z);
+        if(playerpos.position.y - tf.position.y > cameraposDiff )
+        {
+            tf.position = new Vector3(tf.position.x, tf.position.y + 0.04f, tf.position.z);
+        }
+        if(playerpos.position.y-tf.position.y < -cameraposDiff )
+        {
+            tf.position = new Vector3(tf.position.x, tf.position.y - 0.04f, tf.position.z);
+        }
     }
     // Start is called before the first frame update
     void Start()
