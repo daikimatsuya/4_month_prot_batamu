778c5e8d108655d08ab7c5a36c7d877197bbfb6d
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2024-02-28 11:36:09 +0900

    アイテム追加

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private void ItemController()
+    {
+
+    }
+    private void GetItem()
+    {
+        if (this.tag == "Item")
+        {
+
+        }
+        Destroy(this.gameObject);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            GetItem();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
