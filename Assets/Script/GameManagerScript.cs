2ba9959fd74498febaac137bd05f02c24a81f0f9
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2024-03-11 15:31:56 +0900

    ステージを移動できるように

--- Assets/Script/GameManagerScript.cs
+++ Assets/Script/GameManagerScript.cs
@@ -1,17 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour
 {
     //１ステージ　チュートリアル
     //２ステージ　反転をうまく使うといいスコアになる
-    //３ステージ　ボーナス　おふざけ　
+    //３ステージ　風船紹介
     //４ステージ　アクションをうまく使ってクリアする　難度が一番高い
+
+    [SerializeField] string stage1;
+    [SerializeField] string stage2;
+    [SerializeField] string stage3;
+    [SerializeField] string stage4;
+
     private int playerHp;
     private int getItem;
     private bool isGoal;
 
+
+    private void GameController()
+    {
+        ChangeStage();
+    }
+    private void ChangeStage()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(stage1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene(stage2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene(stage3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene(stage4);
+        }
+
+    }
     public void SendPlayerHp(int hp)
     {
         playerHp = hp;
@@ -50,6 +82,6 @@ public class GameManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameController();
     }
 }
